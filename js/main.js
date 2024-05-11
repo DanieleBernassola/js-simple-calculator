5d1fd88f1615d31f59fb9a497173d31cc63bbe2f
DanieleBernassola <danielebernassola02@gmail.com>
2024-05-11 12:32:45 +0200

    [UPD] milestone 3, to fix number clicks

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ const equals = document.querySelector('.numbers .orange');
 let num1 = 0;
 let operator = '';
 let num2 = 0;
+let result = 0;
 
 // Event listener per tutti i numeri
 numbers.addEventListener('click', function(e){
@@ -23,30 +24,59 @@ numbers.addEventListener('click', function(e){
 
 // Event listener per gli operatori
 operators.addEventListener('click', function(e){
-    let operatorSelected = e.target.innerHTML;
-    console.log('Hai cliccato su operatore:', operatorSelected);
-    // Reset schermo
-    screen.innerHTML = ``;
-
-    switch (operatorSelected){
-        case '+':
-            operator = '+';
-            break
-        case '-':
-            operator = '-';
-            break
-        case 'x':
-            operator = '*';
-            break
-        case '/':
-            operator = '/';
-            break
+    if (e.target.className === 'operator'){
+        let operatorSelected = e.target.innerHTML;
+        console.log('Hai cliccato su operatore:', operatorSelected);
+        // Reset schermo
+        screen.innerHTML = ``;
+    
+        switch (operatorSelected){
+            case '+':
+                operator = '+';
+                break
+            case '-':
+                operator = '-';
+                break
+            case 'x':
+                operator = '*';
+                break
+            case '/':
+                operator = '/';
+                break
+        }
+        console.log('Questo è il primo operando:', num1);
+        console.log('Hai scelto operatore:', operator);
     }
-    console.log('Questo è il primo operando:', num1);
-    console.log('Hai scelto operatore:', operator);
 })
 
+numbers.addEventListener('click', function(e){
+    if (e.target.className === 'number' || e.target.className === 'large number'){
+        let numberSelected = parseInt(e.target.innerHTML);
+        console.log('Hai cliccato su:', numberSelected);
+        // String interpolation sul result(schermo calcolatrice)
+        screen.innerHTML += `${numberSelected}`;
+        num2 = numberSelected;
+        console.log(num2);
+    }
+})
 
-// equals.addEventListener('click', function(e){
-//     let equal = e.target.innerHTML;
-// })
+equals.addEventListener('click', function(e){
+    console.log('Secondo operando', num2);
+    console.log('Calcolo operazione...');
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+    }
+    console.log(result);
+    screen.innerHTML = result;
+})
